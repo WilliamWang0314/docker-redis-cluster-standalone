b43cc9bda64e2f9a92980cc1197f595702859081
WilliamWang0314 <2623047617@qq.com>
2019-05-25 17:40:12 +0800

    Revert "2623047617@qq.com"
    This reverts commit 171e0986837316d7356fc2cb0ba5449a7785be23.

--- mem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/usr/local/bin/redis-server /data/nosql/redis_7005/redis_7005.conf
-/usr/local/bin/redis-server /data/nosql/redis_7004/redis_7004.conf
-/usr/local/bin/redis-server /data/nosql/redis_7006/redis_7006.conf
-/usr/local/bin/redis-server /data/nosql/redis_7001/redis_7001.conf
-/usr/local/bin/redis-server /data/nosql/redis_7002/redis_7002.conf
-/usr/local/bin/redis-server /data/nosql/redis_7003/redis_7003.conf
-tail -f /data/nosql/redis_7001/redis.log
